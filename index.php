458b6d79c6243ed1209dd8d0a0bddad0b62a956b
ErwannLG <e.legall@codeur.online>
2019-10-01 12:30:04 +0200

    merge fixes

--- index.php
+++ index.php
@@ -6,12 +6,6 @@
     <title>Bomberman</title>
     <link rel="stylesheet" href="style.css">
   </head>
-
-<<<<<<< HEAD
-    <script src="js/player.js" charset="utf-8"></script>
-    <script src="js/script.js" charset="utf-8"></script>
-    <script src="js/terrain.js" charset="utf-8"></script>
-=======
   <body>
     <section id="bomberman">
       <div id="conteneur">
@@ -19,11 +13,13 @@
       </div>
     </section>
 
+
+
     <script src="js/script.js" charset="utf-8"></script>
     <script src="js/terrain.js" charset="utf-8"></script>
     <script src="js/bombe.js" charset="utf-8"></script>
     <script src="js/player.js" charset="utf-8"></script>
 
->>>>>>> aeb2ecbf0fc8846ed26adccaeab4dbbebf4ac3e8
+
   </body>
 </html>
