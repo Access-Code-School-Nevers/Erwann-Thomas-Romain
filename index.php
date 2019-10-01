f7e2f244df7890509818afdb188c307cab586c9a
Thomas-Gallusser <t.gallusser@codeur.online>
2019-10-01 11:12:28 +0200

    merge all

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+
   <head>
     <meta charset="utf-8">
     <title>Bomberman</title>
     <link rel="stylesheet" href="style.css">
-
   </head>
+
   <body>
-<section id="bomberman">
-  <div id="conteneur">
-    <div id="player">
-      <img src="./img/don_idle.png" alt="player">
-    </div>
-  </div>
-</section>
-
-
+    <section id="bomberman">
+      <div id="conteneur">
+        <div id="player">
+          <img src="./img/don_idle.png" alt="player">
+        </div>
+      </div>
+    </section>
 
     <script src="js/script.js" charset="utf-8"></script>
     <script src="js/terrain.js" charset="utf-8"></script>
